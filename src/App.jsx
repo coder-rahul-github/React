9e3d9d31756c7b5d28d356b71b3eabad982ade09
RAHUL DAS <dasr6163@gmail.com>
2026-07-23 23:39:45 +0530

    Fix Button import casing and add Button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import './App.css'
+import Button from "./button";
 
 function App() {
 
   return (
-    <h1 className='bg-red-500 text-white p-8'>Hello World</h1>
+    <>
+      <h1 className='text-5xl text-blue-500'>Hello World</h1>
+      <Button />
+
+    </>
   );
 }
 
